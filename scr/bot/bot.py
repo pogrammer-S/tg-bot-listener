2b7b128ea2d677c2a4934aee9f4da0eb7761f500
pogrammer-S <ilarumancev69@gmail.com>
2025-07-15 13:49:13 +0700

    TGB-5 изменения 3
    поменять сервисы убрать бота
    сделать pipline тестовый(обработка сообщений)
    перенести init в docker или docker-compose

--- scr/bot/bot.py
+++ scr/bot/bot.py
@@ -1,11 +1,11 @@
 import telebot
 import os
 from dotenv import load_dotenv
-from scr.database.db_service import test_analysis
+from scr.scheduler.pipline import returm_analysis_lmm
 
 load_dotenv()
 
 bot = telebot.TeleBot(os.getenv("TOKEN"))
 
-def Analysis():
-    test_analysis(bot, os.getenv("Test_chat"))
+def return_analysis_in_chat():
+    bot.send_message(os.getenv("Test_chat"), returm_analysis_lmm())
--- scr/bot/listener.py
+++ scr/bot/listener.py
@@ -1,4 +1,4 @@
-from scr.database.db_service import save_message, new_user
+from scr.database.db_service import store_message, store_user
 from scr.bot.bot import bot
 
 @bot.message_handler(content_types=['text'])
@@ -6,8 +6,8 @@ def new_message(message):
     if not message.text:
         return
     
-    new_user(message.from_user.id, message.from_user.username, message.date)
-    save_message(message.from_user.id, message.text, message.date)
+    store_user(message.from_user.id, message.from_user.username, message.date)
+    store_message(message.from_user.id, message.text, message.date)
     
 
     bot.reply_to(message, "Полученно")
