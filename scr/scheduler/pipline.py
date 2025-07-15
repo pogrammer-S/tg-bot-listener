eb020a6dc42e5e771c2f2634373402d879802ded
pogrammer-S <ilarumancev69@gmail.com>
2025-07-15 14:06:16 +0700

    TGB-5 исправление 3.2
    исправление ошибки в pipline

--- scr/scheduler/pipline.py
+++ scr/scheduler/pipline.py
@@ -1,7 +1,7 @@
 from scr.database.db_service import get_analysis_data
 
 def returm_analysis_lmm():
-    return get_analysis_data()
+    return provide_analysis(get_analysis_data())
 
 def provide_analysis(data_from_lmm):
     return data_from_lmm
