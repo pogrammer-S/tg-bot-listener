c11e308c8f34c4edb030a28d7ce05529e88c987f
Евгений Краус <evgeniykraus@gmail.com>
2025-07-16 11:17:51 +0300

    Доработки общей точки входа в приложение

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from scr.bot.bot import bot 
-from scr.database.connection import connection
-from scr.bot.listener import *
+from scr.bot.bot import bot
+from scr.scheduler.scheduler import start_scheduler
+import logging
 
-bot.polling()
-connection.close()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
+    start_scheduler()
+    bot.polling(none_stop=True)
