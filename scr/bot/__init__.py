93022214b686cb92ad1c9492f4052847785fdcec
Евгений Краус <evgeniykraus@gmail.com>
2025-07-16 11:16:12 +0300

    Рефакторинг сервиса бота

---
+++ scr/bot/__init__.py
@@ -0,0 +1,9 @@
+from . import bot
+from . import handlers
+from .utils import *
+
+__all__ = [
+    "handlers",
+    "utils",
+    "bot",
+]
--- scr/bot/bot.py
+++ scr/bot/bot.py
@@ -1,11 +1,5 @@
 import telebot
-import os
-from dotenv import load_dotenv
-from scr.scheduler.pipline import returm_analysis_lmm
+from scr.config.config import load_config
 
-load_dotenv()
-
-bot = telebot.TeleBot(os.getenv("TOKEN"))
-
-def return_analysis_in_chat():
-    bot.send_message(os.getenv("Test_chat"), returm_analysis_lmm())
+config = load_config()
+bot = telebot.TeleBot(config.TOKEN)
