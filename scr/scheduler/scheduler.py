55682b9f6388a5320d52bfc9866c551703518b04
pogrammer-S <ilarumancev69@gmail.com>
2025-07-23 10:17:12 +0700

    TGB-13  Финальный рефакторинг и отладка всего приложения

--- scr/scheduler/scheduler.py
+++ scr/scheduler/scheduler.py
@@ -3,7 +3,6 @@ from scr.scheduler.pipline import run_weekly_pipeline
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
-    #scheduler.add_job(run_weekly_pipeline, 'cron', day_of_week='mon', hour=9)
-    scheduler.add_job(run_weekly_pipeline, "interval", seconds=30)
+    scheduler.add_job(run_weekly_pipeline, 'cron', day_of_week='mon', hour=9)
     scheduler.start()
     return scheduler
